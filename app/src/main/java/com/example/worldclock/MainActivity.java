4ec0982f09b95b1e4aef487b52235f2780cb944b
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-01-20 22:22:45 +0530

    sorted

--- app/src/main/java/com/example/worldclock/MainActivity.java
+++ app/src/main/java/com/example/worldclock/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.worldclock;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> arr=new ArrayList<>();
+        Set<String> timezones=ZoneId.getAvailableZoneIds();
+        timezones
+                .stream().sorted()
+                .forEach(tz->{
+            ZonedDateTime obj= ZonedDateTime.now(ZoneId.of(tz));
+            arr.add(tz+" :"+obj.getHour()+":"+obj.getMinute()+":"+obj.getSecond()+" "+obj.getDayOfMonth()+"/"+obj.getMonthValue()+"/"+obj.getYear());
+        });
+        ArrayList<MyListData> myListData=new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++)
+        {
+            myListData.add(new MyListData(arr.get(i), android.R.drawable.ic_dialog_email));
+        }
+
+//                new MyListData("Info", android.R.drawable.ic_dialog_info),
+//                new MyListData("Delete", android.R.drawable.ic_delete),
+//                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
+//                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
+//                new MyListData("Map", android.R.drawable.ic_dialog_map),
+//                new MyListData("Email", android.R.drawable.ic_dialog_email),
+//                new MyListData("Info", android.R.drawable.ic_dialog_info),
+//                new MyListData("Delete", android.R.drawable.ic_delete),
+//                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
+//                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
+//                new MyListData("Map", android.R.drawable.ic_dialog_map),
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        MyListAdapter adapter = new MyListAdapter(myListData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/worldclock/MyListAdapter.java
@@ -0,0 +1,60 @@
+package com.example.worldclock;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
+    private ArrayList<MyListData> listdata;
+
+    // RecyclerView recyclerView;
+    public MyListAdapter(ArrayList<MyListData> listdata) {
+        this.listdata = listdata;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.activity_recycle_the_view, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final MyListData myListData = listdata.get(position);
+        holder.textView.setText(listdata.get(position).getDescription());
+        holder.imageView.setImageResource(listdata.get(position).getImgId());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"click on item: "+myListData.getDescription(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return listdata.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            this.textView = (TextView) itemView.findViewById(R.id.textView);
+            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.relativeLayout);
+        }
+    }
+}
